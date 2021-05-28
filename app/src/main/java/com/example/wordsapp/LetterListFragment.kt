c096cea1f67ea5ec6ee044c768828597fb213fc0
Pierre <pie.vancin@gmail.com>
2021-05-27 23:17:03 -0300

    wip: migration to fragments

---
+++ app/src/main/java/com/example/wordsapp/LetterListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.wordsapp
+
+import androidx.fragment.app.Fragment
+
+
+class LetterListFragment : Fragment() {
+
+}
---
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -0,0 +1,12 @@
+package com.example.wordsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class WordListFragment : Fragment() {
+
+}
