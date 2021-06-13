573aa0cc2f5c2ab18b0c88fb06f03ed9052e1621
Sander Knutsen <sanderkn@uio.no>
2021-06-13 22:24:22 +0200

    Made the intent stuff better

--- app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -66,7 +66,7 @@ class LetterAdapter :
         holder.button.setOnClickListener {
             val context = holder.view.context
             val intent = Intent(context, DetailActivity::class.java)
-            intent.putExtra("letter", holder.button.text.toString())
+            intent.putExtra(DetailActivity.LETTER, holder.button.text.toString())
             context.startActivity(intent)
         }
     }
