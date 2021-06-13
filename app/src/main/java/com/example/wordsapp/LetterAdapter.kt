19f2a09b5e9ee0b0854376d1077fe53f65328a4d
Sander Knutsen <sanderkn@uio.no>
2021-06-13 22:06:46 +0200

    Implementet explicit intent in onBindViewHolder

--- app/src/main/java/com/example/wordsapp/LetterAdapter.kt
+++ app/src/main/java/com/example/wordsapp/LetterAdapter.kt
@@ -15,6 +15,7 @@
  */
 package com.example.wordsapp
 
+import android.content.Intent
 import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
@@ -62,6 +63,12 @@ class LetterAdapter :
     override fun onBindViewHolder(holder: LetterViewHolder, position: Int) {
         val item = list.get(position)
         holder.button.text = item.toString()
+        holder.button.setOnClickListener {
+            val context = holder.view.context
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("letter", holder.button.text.toString())
+            context.startActivity(intent)
+        }
     }
 
     // Setup custom accessibility delegate to set the text read with
