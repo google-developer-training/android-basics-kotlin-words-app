dd75624f9afa05446e557de6bb1d918f7e259a6a
schordas <schordas@google.com>
2022-09-30 11:19:40 -0700

    Update deps, refactor overloads, update test

--- app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -9,12 +9,9 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class NavigationTests {
 
     @Test
