214c8372f09b61c3acdec9e33a2ab1cb6bb59acd
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-05-11 01:50:04 +0600

    Create Instumentation tests

---
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -0,0 +1,38 @@
+package com.example.wordsapp
+
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class NavigationTests {
+
+    @Test
+    fun navigate_to_words_nav_component() {
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+        val letterListScenario =
+            launchFragmentInContainer<LetterListFragment>(themeResId = R.style.Theme_Words)
+        letterListScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                2,
+                click()
+            )
+        )
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+    }
+}
