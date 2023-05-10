39bd297fc5d3de8e3df29bd9e3fa12ebcaf95fc3
yerakulmanov <200103127@stu.sdu.edu.kz>
2023-05-11 02:02:48 +0600

    Avoid repeat code with annotations

--- app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -1,5 +1,6 @@
 package com.example.wordsapp
 
+import androidx.fragment.app.testing.FragmentScenario
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.Navigation
 import androidx.navigation.testing.TestNavHostController
@@ -9,10 +10,76 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.AfterClass
 import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Test
+import org.junit.runner.RunWith
 
 class NavigationTests {
+
+//    private lateinit var navController: TestNavHostController
+//
+//    private lateinit var exampleFragmentScenario: FragmentScenario<ExampleFragment>
+
+//    @Before
+//    fun setup(){
+//        navController = TestNavHostController(
+//            ApplicationProvider.getApplicationContext()
+//        )
+//
+//        exampleFragmentScenario =  launchFragmentInContainer(themeResId=R.style.Theme_Example)
+//
+//        exampleFragmentScenario.onFragment { fragment ->
+//
+//            navController.setGraph(R.navigation.example_nav_graph)
+//
+//            Navigation.setViewNavController(fragment.requireView(),  navController)
+//        }
+//    }
+
+    @RunWith(AndroidJUnit4::class)
+    class OrderOfTestAnnotations{
+        @Before
+        fun setupFunction(){
+            println("Set up function")
+        }
+
+        @Test
+        fun test_a(){
+            println("Test a")
+        }
+        @Test
+        fun test_b(){
+            println("Test b")
+        }
+        @Test
+        fun test_c(){
+            println("Test c")
+        }
+        @After
+        fun tearDownFunction(){
+            println("Tear down function")
+        }
+
+        companion object{
+            @BeforeClass
+            @JvmStatic
+            fun setupClass(){
+                println("Set up class")
+            }
+            @AfterClass
+            @JvmStatic
+            fun tearDownClass(){
+                println("Tear Down class")
+            }
+        }
+
+
+    }
 
     @Test
     fun navigate_to_words_nav_component() {
